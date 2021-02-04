dbb10005f3f719668237aa91f114714d5b232ff8
Izalpha <visualstudio530@naver.com>
2021-02-04 19:59:50 +0900

    clock done

--- clock.js
+++ clock.js
@@ -1,6 +1,13 @@
+const clock = document.querySelector(".js-clock");
 
+function printClock() {
+    const current = new Date();
+    clock.innerText = 
+    `${current.getHours() < 10 ? `0${current.getHours()}` : current.getHours()}:${current.getMinutes() < 10 ? `0${current.getMinutes()}` : current.getMinutes()}:${current.getSeconds() < 10 ? `0${current.getSeconds()}` : current.getSeconds()}`
+}
 
 function init() {
-
+    printClock();
+    setInterval(printClock, 1000);
 }
 init();
