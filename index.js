1970100f047cf63f408d1403d38711d8c4e32631
Izalpha <visualstudio530@naver.com>
2021-02-21 03:54:00 +0900

    omg

--- index.js
+++ index.js
@@ -1,41 +1,172 @@
-const submitBtn = document.querySelector(".submitBtn");
-const numberBar = document.querySelector(".numberBar");
-const numberVers = document.querySelector(".numberVers");
-const selectedRange = document.querySelector(".selectedRange");
-const inputNumber = document.querySelector(".inputNumber");
-const result = document.querySelector(".result");
+const numberBar = document.querySelector(".calculator__numberBar");
+const button1 = document.getElementById('1');
+const button2 = document.getElementById("2");
+const button3 = document.getElementById("3");
+const button4 = document.getElementById("4");
+const button5 = document.getElementById("5");
+const button6 = document.getElementById("6");
+const button7 = document.getElementById("7");
+const button8 = document.getElementById("8");
+const button9 = document.getElementById("9");
+const button0 = document.getElementById("0");
+const buttonPlus = document.getElementById("+");
+const buttonMinus = document.getElementById("-");
+const buttonMultyfly = document.getElementById("*");
+const buttonDivide = document.getElementById("/");
+const buttonC = document.getElementById("c");
+const buttonEqual = document.getElementById("=");
+let isOperOn = null;
+let savedNum = -1;
+let currentNum = -1;
+let count = false;
+let resultNum = false;
 
-function handledrag(event) {
-    selectedRange.innerText = `Generate a number between 0 and ${numberBar.value}`;
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-    paintResult();
-}
-function paintResult() {
-    numberVers.classList.remove("hidden");
-    result.classList.remove("hidden");
-    const RAMDOM_RANGE = numberBar.value;
-    const myNum = inputNumber.value;
-    const machineNum = genRandNumber(RAMDOM_RANGE);
-    numberVers.innerText = `You chose: ${myNum}, the machine chose: ${machineNum}.`
-    console.log(myNum, machineNum);
-    if(parseInt(myNum) === parseInt(machineNum)) {
-        result.innerText = "You Won!"
-    } else {
-        result.innerText = "You lost!"
+function clickHandler(event) {
+    const btn = event.target; 
+    if(btn.id >= 0 && btn.id <= 9){
+        if(isOperOn !== null) {
+            if(count === false || numberBar.innerText === "0"){
+                numberBar.innerText = btn.id;
+                currentNum = numberBar.innerText;
+            }
+            else{
+                numberBar.innerText += btn.id;
+                currentNum = numberBar.innerText;
+            }
+            count = true;
+        }else{
+            if(numberBar.innerText === "0"){
+                numberBar.innerText = btn.id;
+                savedNum = numberBar.innerText;
+            }else {
+                numberBar.innerText += btn.id;
+                savedNum = numberBar.innerText;
+            }
+        }
+    }
+    else if(btn.id === "c"){
+        numberBar.innerText = "0";
+        savedNum = -1;
+        currentNum = -1;
+        count = false;
+        resultNum = false;
+        isOperOn = null;
+    }
+    else if(btn.id === "+" && savedNum != -1){
+        if(savedNum !== -1 && currentNum !== -1){
+            bugfix();
+        }
+        isOperOn = '+';
+        count = false;
+    }
+    else if(btn.id === "-" && savedNum != -1){
+        if(savedNum !== -1 && currentNum !== -1){
+            bugfix();
+        }
+        isOperOn = '-';
+        count = false;
+    }
+    else if(btn.id === "*" && savedNum != -1){
+        if(savedNum !== -1 && currentNum !== -1){
+            bugfix();
+        }
+        isOperOn = '*';
+        count = false;
+    }
+    else if(btn.id === "/" && savedNum != -1){
+        if(savedNum !== -1 && currentNum !== -1){
+            bugfix();
+        }
+        isOperOn = '/';
+        count = false;
+    }
+    else {
+        if(isOperOn === '+' && currentNum != -1 && resultNum !== true){
+            numberBar.innerText = parseInt(savedNum) + parseInt(currentNum);
+            savedNum = numberBar.innerText;
+            currentNum = -1;
+            count = false;
+            resultNum = false;
+            isOperOn = null;
+        } 
+        else if(isOperOn === '-' && currentNum != -1 && resultNum !== true){
+            numberBar.innerText = parseInt(savedNum) - parseInt(currentNum);
+            savedNum = numberBar.innerText;
+            currentNum = -1;
+            count = false;
+            resultNum = false;
+            isOperOn = null;
+        }
+        else if(isOperOn === '*' && currentNum != -1 && resultNum !== true){
+            numberBar.innerText = parseInt(savedNum) * parseInt(currentNum);
+            savedNum = numberBar.innerText;
+            currentNum = -1;
+            count = false;
+            resultNum = false;
+            isOperOn = null;
+        }
+        else if(isOperOn === '/' && currentNum != -1 && resultNum !== true){
+            numberBar.innerText = parseInt(savedNum) / parseInt(currentNum);
+            savedNum = numberBar.innerText;
+            currentNum = -1;
+            count = false;
+            resultNum = false;
+            isOperOn = null;
+        }
     }
 }
 
-function genRandNumber(RAMDOM_RANGE) {
-    const num = Math.floor(Math.random() * RAMDOM_RANGE);
-    return num;
+function bugfix() {
+    if(isOperOn === '+'){
+        numberBar.innerText = parseInt(savedNum) + parseInt(currentNum);
+        savedNum = numberBar.innerText ;
+        currentNum = -1;
+        count = false;
+        resultNum = false;
+        isOperOn = null;
+    } 
+    else if(isOperOn === '-'){
+        numberBar.innerText = parseInt(savedNum) - parseInt(currentNum);
+        savedNum = numberBar.innerText ;
+        currentNum = -1;
+        count = false;
+        resultNum = false;
+        isOperOn = null;
+    }
+    else if(isOperOn === '*'){
+        numberBar.innerText = parseInt(savedNum) * parseInt(currentNum);
+        savedNum = numberBar.innerText ;
+        currentNum = -1;
+        count = false;
+        resultNum = false;
+        isOperOn = null;
+    }
+    else if(isOperOn === '/'){
+        numberBar.innerText = parseInt(savedNum) / parseInt(currentNum);
+        savedNum = numberBar.innerText ;
+        currentNum = -1;
+        count = false;
+        resultNum = false;
+        isOperOn = null;
+    }
 }
 
 function init() {
-    selectedRange.innerText = `Generate a number between 0 and ${numberBar.value}`;
-    submitBtn.addEventListener("click", handleSubmit);
-    numberBar.addEventListener("input", handledrag);
+    button0.addEventListener("click",clickHandler);
+    button1.addEventListener("click",clickHandler);
+    button2.addEventListener("click",clickHandler);
+    button3.addEventListener("click",clickHandler);
+    button4.addEventListener("click",clickHandler);
+    button5.addEventListener("click",clickHandler);
+    button6.addEventListener("click",clickHandler);
+    button7.addEventListener("click",clickHandler);
+    button8.addEventListener("click",clickHandler);
+    button9.addEventListener("click",clickHandler);
+    buttonC.addEventListener("click",clickHandler);
+    buttonPlus.addEventListener("click",clickHandler);
+    buttonMinus.addEventListener("click",clickHandler);
+    buttonMultyfly.addEventListener("click",clickHandler);
+    buttonDivide.addEventListener("click",clickHandler);
+    buttonEqual.addEventListener("click",clickHandler);
 }
 init();
